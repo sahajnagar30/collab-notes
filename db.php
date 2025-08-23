2b50aacafd28bcadc2f464cdec017a33f91d879b
Sahaj <nagars.iitk@gmail.com>
2025-08-23 21:08:48 +0530

    Re-added db.php

--- db.php
+++ db.php
@@ -3,9 +3,9 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $host = "localhost";
-$user = "root";   // default in XAMPP
-$pass = "";       // default is empty
-$db   = "notes_app"; // the database we created in phpMyAdmin
+$user = "root";   
+$pass = "";       
+$db   = "notes_app_test"; 
 
 $conn = new mysqli($host, $user, $pass, $db);
 
